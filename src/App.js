a8675be560c5cbbd2fa9a0226bbae7d36e327075
SofiaFredriksson <sofiapkf@gmail.com>
2019-04-19 11:16:16 +0100

    almost done

--- src/App.js
+++ src/App.js
@@ -3,12 +3,63 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzas: [],
+    selectedPizza: {},
+    topping: '',
+    size: '',
+    vegetarian: false
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pizzas')
+    .then(resp => resp.json())
+    .then(data => this.setState({
+      pizzas: data
+    }))
+  }
+
+  handleEditClick = (id) => {
+    const pizza = this.state.pizzas.find(pizza => pizza.id === id)
+    this.setState({
+      topping: pizza.topping,
+      size: pizza.size,
+      vegetarian: pizza.vegetarian
+    })
+
+  }
+
+  handleInputChange = (event) => {
+    console.log('hello');
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleRadioChange = (event) => {
+    console.log('hello');
+    if(event.target.value === 'Vegetarian'){
+    this.setState({vegetarian: true})
+    } else {
+    this.setState({vegetarian: false})
+    }
+  }
+
+  handleSubmit = () => {
+    console.log('hello');
+  }
+
+
   render() {
+    console.log(this.state.topping);
+    console.log(this.state.size);
+    console.log(this.state.vegetarian);
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm handleSubmit={this.handleSubmit} handleRadioChange={this.handleRadioChange} topping={this.state.topping} vegetarian={this.state.vegetarian} size={this.state.size} handleInputChange={this.handleInputChange}/>
+        <PizzaList pizzas={this.state.pizzas} handleEditClick={this.handleEditClick}/>
       </Fragment>
     );
   }
